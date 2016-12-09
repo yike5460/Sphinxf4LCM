cd50edc180b6689e7df7a91eda624d6595120506
Alex Carp <acarp@luxoft.com>
2016-12-09 15:06:21 +0200

    Added create_vnf_id stub

--- api/generic/vnfm.py
+++ api/generic/vnfm.py
@@ -1,6 +1,12 @@
-class Vnfm:
+from api.adapter import construct_vnfm_adapter
+
+class Vnfm(object):
     def __init__(self, vendor = None, **kwargs):
         self.vendor = vendor
+        self.vnfm_adapter = construct_vnfm_adapter(vendor)
 
     def vnf_instantiate(self):
-        pass
+        return self.vnfm_adapter.vnf_instantiate()
+
+    def create_vnf_id(self, vnfd_id, vnf_instance_name = None, vnf_instance_description = None):
+        return self.vnfm_adapter.create_vnf_id(vnfd_id, vnf_instance_name, vnf_instance_description)
