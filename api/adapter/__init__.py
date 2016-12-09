5187cbe6652ae7b2cb72fd45ae496b86fe5e4750
Alex Carp <acarp@luxoft.com>
2016-12-09 15:09:25 +0200

    Added adapter logic

---
+++ api/adapter/__init__.py
@@ -0,0 +1,5 @@
+from api.adapter.openstack.vnfm import VnfmOpenstackAdapter
+
+def construct_vnfm_adapter(vendor):
+    if (vendor == "openstack"):
+        return VnfmOpenstackAdapter()
---
+++ api/adapter/openstack/vnfm.py
@@ -0,0 +1,9 @@
+class VnfmOpenstackAdapter(object):
+    def __init__(self):
+        pass
+
+    def vnf_instantiate(self):
+        pass
+
+    def create_vnf_id(self, vnfd_id, vnf_instance_name, vnf_instance_description):
+        pass
