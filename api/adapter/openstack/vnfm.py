4a93997e3a32c6e2a2102f69fb36cd67684c6b8f
Alex Carp <acarp@luxoft.com>
2016-12-23 14:56:51 +0200

    Added vnf_query function for openstack

--- api/adapter/openstack/vnfm.py
+++ api/adapter/openstack/vnfm.py
@@ -6,7 +6,6 @@ from tackerclient.tacker.client import Client as TackerClient
 from api.generic import constants
 import tackerclient.common.exceptions
 
-
 LOG = logging.getLogger(__name__)
 
 
@@ -33,19 +32,19 @@ class VnfmOpenstackAdapter(object):
         LOG.warning('"Lifecycle Operation Occurence Id" is not implemented in OpenStack!')
         LOG.warning('Will return the state of the resource with given Id')
 
+        # TODO: call etsi standard function
         vnf_id = lifecycle_operation_occurrence_id
         tacker_show_vnf = self.tacker_client.show_vnf(vnf_id)
 
         tacker_status = tacker_show_vnf['vnf']['status']
 
-        return constants.OPENSTACK_VNF_STATES_MAPPING[tacker_status]
+        return constants.OPERATION_STATUS['OPENSTACK_VNF_STATE'][tacker_status]
 
     def vnf_instantiate(self, vnf_instance_id, flavour_id, instantiation_level_id=None, ext_virtual_link=None,
                         ext_managed_virtual_link=None, localization_language=None, additional_param=None):
         LOG.warning('"VNF Instantiate" operation is not implemented in OpenStack!')
         LOG.warning('Instead of "Lifecycle Operation Occurence Id", will just return the "VNF Instance Id"')
         return vnf_instance_id
-
 
     @log_entry_exit(LOG)
     def vnf_create_id(self, vnfd_id, vnf_instance_name, vnf_instance_description, **kwargs):
@@ -59,3 +58,37 @@ class VnfmOpenstackAdapter(object):
             return None
         return vnf_instance['vnf']['id']
 
+    @log_entry_exit(LOG)
+    def vnf_query(self, filter, attribute_selector=None):
+        """
+        This operation provides information about VNF instances. The applicable VNF instances can be chosen based on
+        filtering criteria, and the information can be restricted to selected attributes.
+
+        This function was written in accordance with section 7.2.9 of GS NFV-IFA 007 - v2.1.1.
+
+        :param filter:              Filter to select the VNF instance(s) about which information is queried.
+        :param attribute_selector:  Provides a list of attribute names. If present, only these attributes are returned
+                                    for the VNF instance(s) matching the filter. If absent, the complete information is
+                                    returned for the VNF instance(s) matching the filter.
+        :return:                    The information items about the selected VNF instance(s) that are returned. If
+                                    attribute_selector is present, only the attributes listed in attribute_selector are
+                                    returned for the selected VNF instance(s).
+        """
+        vnf_instance_id = filter['vnf_instance_id']
+        vnf_info = dict()
+        vnf_info['vnf_instance_id'] = vnf_instance_id
+
+        try:
+            tacker_show_vnf = self.tacker_client.show_vnf(vnf_instance_id)['vnf']
+        except tackerclient.common.exceptions.TackerException:
+            return vnf_info
+
+        vnf_info['vnf_instance_name'] = tacker_show_vnf['name']
+        vnf_info['vnf_instance_description'] = tacker_show_vnf['description']
+        vnf_info['vnfd_id'] = tacker_show_vnf['vnfd_id']
+        vnf_info['instantiation_state'] = constants.VNF_INSTANTIATION_STATE['OPENSTACK_VNF_STATE'][tacker_show_vnf['status']]
+
+        vnf_info['instantiated_vnf_info'] = dict()
+        vnf_info['instantiated_vnf_info']['vnf_state'] = constants.VNF_STATE['OPENSTACK_VNF_STATE'][tacker_show_vnf['status']]
+
+        return vnf_info
