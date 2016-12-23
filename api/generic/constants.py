47b00c7cdaa38c166ff416c2a9ae2369f03288f2
Alex Carp <acarp@luxoft.com>
2016-12-23 14:56:08 +0200

    Updated constants

--- api/generic/constants.py
+++ api/generic/constants.py
@@ -3,7 +3,26 @@ OPERATION_FAILED = 'FAILED'
 OPERATION_PENDING = 'PENDING'
 OPERATION_FINAL_STATES = [OPERATION_SUCCESS, OPERATION_FAILED]
 
-OPENSTACK_VNF_STATES_MAPPING = {'ACTIVE': OPERATION_SUCCESS,
-                                'ERROR': OPERATION_FAILED,
-                                'PENDING_CREATE': OPERATION_PENDING,
-                                'PENDING_DELETE': OPERATION_PENDING}
+VNF_INSTANTIATED = 'INSTANTIATED'
+VNF_NOT_INSTANTIATED = 'NOT_INSTANTIATED'
+
+VNF_STARTED = 'STARTED'
+VNF_STOPPED = 'STOPPED'
+
+OPERATION_STATUS = dict()
+OPERATION_STATUS['OPENSTACK_VNF_STATE'] = {'ACTIVE': OPERATION_SUCCESS,
+                                           'ERROR': OPERATION_FAILED,
+                                           'PENDING_CREATE': OPERATION_PENDING,
+                                           'PENDING_DELETE': OPERATION_PENDING}
+
+VNF_INSTANTIATION_STATE = dict()
+VNF_INSTANTIATION_STATE['OPENSTACK_VNF_STATE'] = {'ACTIVE': VNF_INSTANTIATED,
+                                                  'ERROR': VNF_NOT_INSTANTIATED,
+                                                  'PENDING_CREATE': VNF_NOT_INSTANTIATED,
+                                                  'PENDING_DELETE': VNF_NOT_INSTANTIATED}
+
+VNF_STATE = dict()
+VNF_STATE['OPENSTACK_VNF_STATE'] = {'ACTIVE': VNF_STARTED,
+                                    'ERROR': VNF_STOPPED,
+                                    'PENDING_CREATE': VNF_STOPPED,
+                                    'PENDING_DELETE': VNF_STOPPED}
