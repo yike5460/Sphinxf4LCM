dc05099d1523ffb33dc01e3eb7cc9e01f5d9fb94
Alex Carp <acarp@luxoft.com>
2016-12-30 16:18:38 +0200

    Added TestCase metaclass for generic initialization

--- test_cases/__init__.py
+++ test_cases/__init__.py
@@ -1,14 +1,29 @@
+import importlib
+from utils.logging_module import configure_logger
+
+
 class TestExecutionError(Exception):
     pass
 
 
+class TestMeta(type):
+    def __new__(meta, name, bases, class_dict):
+        if bases != (object,):
+            originating_module = importlib.import_module(class_dict['__module__'])
+            class_dict['_LOG'] = originating_module.LOG
+        return type.__new__(meta, name, bases, class_dict)
+
+
 class TestCase(object):
+    __metaclass__ = TestMeta
+
     def __init__(self, tc_input):
         self.tc_input = tc_input
         self.tc_result = {}
 
-    def initialize(self):
-        pass
+    @classmethod
+    def initialize(cls):
+        configure_logger(cls._LOG, file_level='DEBUG', console_level='INFO', override_parent=True)
 
     def setup(self):
         return True
