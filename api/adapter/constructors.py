fe1f217a2d21091e351ef5239e176882823831a6
Alex Carp <acarp@luxoft.com>
2017-01-17 12:28:57 +0200

    Replaced 'not' with 'is None'

--- api/adapter/constructors.py
+++ api/adapter/constructors.py
@@ -7,7 +7,7 @@ _vendor_constructor_mapping = None
 
 def get_vendor_constructor_mapping():
     global _vendor_constructor_mapping
-    if not _vendor_constructor_mapping:
+    if _vendor_constructor_mapping is None:
         mapping_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), mapping_file_name)
         with open(mapping_file_path, 'r') as mapping_file:
             _vendor_constructor_mapping = json.load(mapping_file)
