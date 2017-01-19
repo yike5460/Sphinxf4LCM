66c7ea94e3d7b762bf4492bc3392f5059924f9a2
Alex Carp <acarp@luxoft.com>
2017-01-19 15:05:55 +0200

    Added code for dynamically handling data model schemas

---
+++ api/structures/structures.py
@@ -0,0 +1,119 @@
+import os
+import sys
+import yaml
+from weakref import WeakKeyDictionary
+
+
+schema_file_name = 'structures.yaml'
+_schema = None
+this_module = sys.modules[__name__]
+
+
+def _load_schema_from_file():
+    global _schema
+    if _schema is None:
+        schema_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), schema_file_name)
+        with open(schema_file_path, 'r') as schema_file:
+            _schema = yaml.load(schema_file)
+    return _schema
+
+
+class Attribute(object):
+    TYPE = NotImplemented
+
+    def __init__(self):
+        self._values = WeakKeyDictionary()
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        else:
+            return self._values[instance]
+
+    def __set__(self, instance, value):
+        self._validate(value)
+        self._values[instance] = value
+
+    @classmethod
+    def _validate(cls, value):
+        if cls.TYPE is NotImplemented:
+            raise NotImplementedError
+        if not isinstance(value, cls.TYPE):
+            raise ValueError('value %s should be of type: %s' % (repr(value), repr(cls.TYPE)))
+
+
+class List(Attribute):
+    TYPE = list
+
+    def __init__(self, entry_type):
+        if not issubclass(entry_type, Attribute):
+            raise TypeError('container entry type should be an Attribute subtype')
+        self.ENTRY_TYPE = entry_type
+        super(List, self).__init__()
+
+    def _validate(self, value):
+        super(List, self)._validate(value)
+        if self.ENTRY_TYPE is NotImplemented:
+            raise NotImplementedError
+        for entry in value:
+            self.ENTRY_TYPE._validate(entry)
+
+
+class Enum(Attribute):
+    def __init__(self, *args):
+        self._allowed_list = args
+        super(Enum, self).__init__()
+
+    def _validate(self, value):
+        if value not in self._allowed_list:
+            raise ValueError('%s value should be in %s' % (value, self._allowed_list))
+
+
+class String(Attribute):
+    TYPE = str
+
+
+class Identifier(String):
+    pass
+
+
+class Version(String):
+    pass
+
+
+class Mapping(Attribute):
+    TYPE = dict
+
+
+class InstantiatedVnfInfo(object):
+    l = List(String)
+
+
+class SchemaLoader(type):
+    def __new__(meta, name, bases, class_dict):
+        if bases != (InformationElement,):
+            class_schema = _load_schema_from_file().get(name)
+
+            for class_attribute_name, class_attribute_schema in class_schema.get('attributes').items():
+                class_attribute_type = class_attribute_schema.get('type')
+                constructor = getattr(this_module, class_attribute_type)
+                class_attribute = constructor()
+                class_dict[class_attribute_name] = class_attribute
+        return type.__new__(meta, name, bases, class_dict)
+
+
+class InformationElement(object):
+    def __setattr__(self, key, value):
+        if key not in type(self).__dict__:
+            raise ValueError('trying to set invalid attribute %s' % key)
+        super(InformationElement, self).__setattr__(key, value)
+
+
+class InformationElementWithExternalSchema(InformationElement):
+    __metaclass__ = SchemaLoader
+
+
+class VnfInfo(InformationElementWithExternalSchema):
+    pass
+
+
