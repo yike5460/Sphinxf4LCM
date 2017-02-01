f2b4ac1d6e0ecab635372b4523586728c92df6ce
Mihai Dragomir <mdragomir@luxoft.com>
2017-02-01 16:59:25 +0200

    Fixed function names

--- api/adapter/dummy/vim.py
+++ api/adapter/dummy/vim.py
@@ -14,11 +14,11 @@ class VimDummyAdapter(object):
         pass
 
     @log_entry_exit(LOG)
-    def vim_query_compute(self, query_compute_filter):
+    def query_virtualised_compute_resource(self, query_compute_filter):
         """
-        This function allows querying information about instantiated virtualized compute resources.
+        This function allows querying information about instantiated virtualised compute resources.
 
-        This function was written in accordance with section 7.3.1.3 and 8.4.3 of GS NFV-IFA 005 - v2.1.1.
+        This function was written in accordance with section 7.3.1.3 and 8.4.3 of ETSI GS NFV-IFA 005 - v2.1.1.
 
         :param query_compute_filter:        Query filter based on e.g. name, identifier, meta-data information or status
                                             information.
@@ -61,11 +61,11 @@ class VimDummyAdapter(object):
         return virtual_compute
 
     @log_entry_exit(LOG)
-    def vim_query_network(self, query_network_filter):
+    def query_virtualised_network_resource(self, query_network_filter):
         """
-        This function allows querying information about instantiated virtualized network resources.
+        This function allows querying information about instantiated virtualised network resources.
 
-        This function was written in accordance with section 7.4.1.3 and 8.4.5 of GS NFV-IFA 005 - v2.1.1.
+        This function was written in accordance with section 7.4.1.3 and 8.4.5 of ETSI GS NFV-IFA 005 - v2.1.1.
 
         :param query_network_filter:        Query filter based on e.g. name, identifier, meta-data information or status
                                             information.
@@ -97,11 +97,11 @@ class VimDummyAdapter(object):
         return virtual_network
 
     @log_entry_exit(LOG)
-    def vim_query_storage(self, query_storage_filter):
+    def query_virtualised_storage_resource(self, query_storage_filter):
         """
-        This function allows querying information about instantiated virtualized storage resources.
+        This function allows querying information about instantiated virtualised storage resources.
 
-        This function was written in accordance with section 7.5.1.3 and 8.4.7 of GS NFV-IFA 005 - v2.1.1.
+        This function was written in accordance with section 7.5.1.3 and 8.4.7 of ETSI GS NFV-IFA 005 - v2.1.1.
 
         :param query_storage_filter:        Query filter based on e.g. name, identifier, meta-data information or status
                                             information.
