42ba35e9c1634b848a26fa442ed6e503df65947b
Alex Carp <acarp@luxoft.com>
2017-02-03 14:56:34 +0200

    Updated function query_virtualised_network_resource

--- api/adapter/openstack/vim.py
+++ api/adapter/openstack/vim.py
@@ -2,6 +2,7 @@ import logging
 
 import os_client_config
 
+from api.structures.objects import VirtualCompute, VirtualCpu, VirtualMemory, VirtualStorage
 from utils.logging_module import log_entry_exit
 
 LOG = logging.getLogger(__name__)
@@ -26,6 +27,27 @@ class VimOpenstackAdapter(object):
 
     @log_entry_exit(LOG)
     def query_virtualised_network_resource(self, filter):
+        virtual_compute = VirtualCompute()
+
         compute_id = filter['compute_id']
+        virtual_compute.compute_id = compute_id
 
         nova_server = self.nova_client.servers.get(compute_id)
+        server_flavor_id = nova_server.flavor['id']
+        virtual_compute.flavour_id = server_flavor_id.encode()
+
+        server_flavor = self.nova_client.flavors.get(server_flavor_id)
+
+        virtual_cpu = VirtualCpu()
+        virtual_cpu.num_virtual_cpu = server_flavor.vcpus
+        virtual_compute.virtual_cpu = virtual_cpu
+
+        virtual_memory = VirtualMemory()
+        virtual_memory.virtual_mem_size = server_flavor.ram
+        virtual_compute.virtual_memory = virtual_memory
+
+        virtual_storage = VirtualStorage()
+        virtual_storage.size_of_storage = server_flavor.disk
+        virtual_compute.virtual_disks = [virtual_storage]
+
+        return virtual_compute
