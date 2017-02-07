339a62f91a5b33d5334716072510eeb9eb8c529f
Mihai Dragomir <mdragomir@luxoft.com>
2017-02-07 13:26:46 +0200

    Updated function calls

--- test_cases/vnf/complex/TC_VNF_COMPLEX_002.py
+++ test_cases/vnf/complex/TC_VNF_COMPLEX_002.py
@@ -35,7 +35,7 @@ class TC_VNF_COMPLEX_002(TestCase):
     def setup(self):
         LOG.info('Starting setup for TC_VNF_COMPLEX_002')
 
-        self.vnfm = Vnfm(vendor=self.tc_input['vnfm_vendor'])
+        self.vnfm = Vnfm(vendor=self.tc_input['vnfm_params']['type'], **self.tc_input['vnfm_params']['client_config'])
         self.vnf = Vnf(vendor=self.tc_input['vnf_vendor'])
         self.traffic = Traffic()
 
@@ -55,9 +55,10 @@ class TC_VNF_COMPLEX_002(TestCase):
         # --------------------------------------------------------------------------------------------------------------
         LOG.info('Instantiating VNF')
         self.time_record.START('instantiate_vnf')
-        self.vnf_instance_id = self.vnfm.vnf_create_and_instantiate(vnfd_id='vnfd_id', flavour_id='123456',
-                                                                    vnf_instance_name='test_vnf',
-                                                                    vnf_instance_description='VNF used for testing')
+        self.vnf_instance_id = self.vnfm.vnf_create_and_instantiate(
+                                                                   vnfd_id=self.tc_input['vnfd_id'], flavour_id=None,
+                                                                   vnf_instance_name=self.tc_input['vnf_instance_name'],
+                                                                   vnf_instance_description=None)
         if self.vnf_instance_id is None:
             LOG.error('TC_VNF_COMPLEX_002 execution failed')
             LOG.debug('Unexpected VNF instantiation ID')
