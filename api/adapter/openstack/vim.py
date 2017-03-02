b1684259e6c06edcb0d227794c44aff64e341736
Mihai Dragomir <mdragomir@luxoft.com>
2017-03-02 17:56:45 +0200

    Added functions port_list() and server_list()

--- api/adapter/openstack/vim.py
+++ api/adapter/openstack/vim.py
@@ -12,7 +12,7 @@ class VimOpenstackAdapter(object):
     def __init__(self, auth_url=None, username=None, password=None, identity_api_version=None, project_name=None,
                  project_domain_name=None, user_domain_name=None, **kwargs):
         try:
-            self.nova_client = os_client_config.make_client('compute',
+            self.heat_client = os_client_config.make_client('orchestration',
                                                             auth_url=auth_url,
                                                             username=username,
                                                             password=password,
@@ -21,7 +21,16 @@ class VimOpenstackAdapter(object):
                                                             project_domain_name=project_domain_name,
                                                             user_domain_name=user_domain_name)
 
-            self.heat_client = os_client_config.make_client('orchestration',
+            self.neutron_client = os_client_config.make_client('network',
+                                                               auth_url=auth_url,
+                                                               username=username,
+                                                               password=password,
+                                                               identity_api_version=identity_api_version,
+                                                               project_name=project_name,
+                                                               project_domain_name=project_domain_name,
+                                                               user_domain_name=user_domain_name)
+
+            self.nova_client = os_client_config.make_client('compute',
                                                             auth_url=auth_url,
                                                             username=username,
                                                             password=password,
@@ -64,7 +73,30 @@ class VimOpenstackAdapter(object):
     @log_entry_exit(LOG)
     def get_stack(self, stack_id):
         """
-        This function gets the metadata for the specified stack ID
+        This function gets the metadata for the specified stack ID.
         """
         stack_state = self.heat_client.stacks.get(stack_id)
         return stack_state
+
+    @log_entry_exit(LOG)
+    def port_list(self, **query_filter):
+        """
+        This function gets the list of ports.
+
+        :param query_filter:    Additional params.
+        :return:                List of ports.
+        """
+        neutron_ports = self.neutron_client.list_ports(retrieve_all=False, **query_filter)
+        return neutron_ports
+
+    @log_entry_exit(LOG)
+    def server_list(self, query_filter=None):
+        """
+        This function gets the list of servers.
+
+        :param query_filter:    Filter out servers which don't match the search_opts (optional). The search opts format
+                                is a dictionary of key / value pairs that will be appended to the query string.
+        :return:                List of servers.
+        """
+        nova_servers = self.nova_client.servers.list(search_opts=query_filter)
+        return nova_servers
