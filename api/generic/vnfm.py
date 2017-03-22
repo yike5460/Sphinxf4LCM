f24f2565be7434ea5c3b289bba2c1201e15629b0
Mihai Dragomir <mdragomir@luxoft.com>
2017-03-22 12:30:13 +0200

    Removed obsolete function

--- api/generic/vnfm.py
+++ api/generic/vnfm.py
@@ -254,37 +254,6 @@ class Vnfm(object):
         return operation_status
 
     @log_entry_exit(LOG)
-    def vnf_scale_to_level_sync(self, vnf_instance_id, instantiation_level_id=None, scale_info=None,
-                                additional_param=None, max_wait_time=constants.INSTANTIATION_TIME,
-                                poll_interval=constants.POLL_INTERVAL):
-        """
-        This function scales synchronously an instantiated VNF of a particular DF to a target size.
-
-        :param vnf_instance_id:         Identifier of the VNF instance to which this scaling request is related.
-        :param instantiation_level_id:  Identifier of the target instantiation level of the current DF to which the
-                                        VNF is requested to be scaled. Either instantiationLevelId or scaleInfo
-                                        but not both shall be present.
-        :param scale_info:              For each scaling aspect of the current DF, defines the target scale level to
-                                        which the VNF is to be scaled. Either instantiationLevelId or scaleInfo
-                                        but not both shall be present.
-        :param additional_param:        Additional parameters passed as input to the scaling process, specific to the
-                                        VNF being scaled.
-        :param max_wait_time:           Maximum interval of time in seconds to wait for the scaling operation to reach a 
-                                        final state.
-        :param poll_interval:           Interval of time in seconds between consecutive polls on the scaling operation 
-                                        result.
-        :return:                        Operation status.
-        """
-        lifecycle_operation_occurrence_id = self.vnf_scale_to_level(vnf_instance_id, instantiation_level_id, scale_info,
-                                                                    additional_param)
-
-        operation_status = self.poll_for_operation_completion(lifecycle_operation_occurrence_id,
-                                                              final_states=constants.OPERATION_FINAL_STATES,
-                                                              max_wait_time=max_wait_time, poll_interval=poll_interval)
-
-        return operation_status
-
-    @log_entry_exit(LOG)
     def vnf_terminate_and_delete(self, vnf_instance_id, termination_type, graceful_termination_type=None,
                                  max_wait_time=constants.INSTANTIATION_TIME, poll_interval=constants.POLL_INTERVAL):
         """
