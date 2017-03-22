5a9d1a138015b9b6f02a0df241fa00be8bdee0c6
Mihai Dragomir <mdragomir@luxoft.com>
2017-03-22 11:39:33 +0200

    Updated function modify_vnf_configuration to poll for VNF status

--- api/adapter/openstack/vnfm.py
+++ api/adapter/openstack/vnfm.py
@@ -1,6 +1,7 @@
 import json
 import logging
 import re
+import time
 import yaml
 
 import os_client_config
@@ -119,9 +120,32 @@ class VnfmOpenstackAdapter(object):
         #     }
         # }
         if vnf_configuration_data is not None:
+            LOG.debug('Sleeping 10 seconds to allow the VNF to boot')
+            time.sleep(10)
             vnf_attributes = {'vnf': {'attributes': {'config': vnf_configuration_data}}}
             self.tacker_client.update_vnf(vnf_instance_id, body=vnf_attributes)
-            return True
+
+        # Poll on the VNF status until it reaches one of the final states
+        operation_pending = True
+        elapsed_time = 0
+        max_wait_time = 300
+        poll_interval = 5
+        lifecycle_operation_occurrence_id = ('vnf', vnf_instance_id)
+        final_states = constants.OPERATION_FINAL_STATES
+
+        while operation_pending and elapsed_time < max_wait_time:
+            operation_status = self.get_operation_status(lifecycle_operation_occurrence_id)
+            LOG.debug('Got status %s for operation with ID %s' % (operation_status, lifecycle_operation_occurrence_id))
+            if operation_status in final_states:
+                operation_pending = False
+            else:
+                LOG.debug('Expected state to be one of %s, got %s' % (final_states, operation_status))
+                LOG.debug('Sleeping %s seconds' % poll_interval)
+                time.sleep(poll_interval)
+                elapsed_time += poll_interval
+                LOG.debug('Elapsed time %s seconds out of %s' % (elapsed_time, max_wait_time))
+
+        return operation_status
 
     @log_entry_exit(LOG)
     def vnf_create_id(self, vnfd_id, vnf_instance_name, vnf_instance_description):
