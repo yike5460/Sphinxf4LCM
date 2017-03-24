1e86de988e83ebcc09a695c52cbf27b0bf1bded5
Mihai Dragomir <mdragomir@luxoft.com>
2017-03-24 11:21:10 +0200

    Added stub functions that check ig config and license were applied for Ubuntu and OpenWrt

---
+++ api/adapter/vnf/ubuntu.py
@@ -0,0 +1,19 @@
+import logging
+
+from utils.logging_module import log_entry_exit
+
+LOG = logging.getLogger(__name__)
+
+
+class UbuntuVnfAdapter(object):
+    def __init__(self):
+        pass
+
+    @log_entry_exit(LOG)
+    def config_applied(self, **credentials):
+
+        return True
+
+    @log_entry_exit(LOG)
+    def license_applied(self, **credentials):
+        return True
--- api/generic/vnf.py
+++ api/generic/vnf.py
@@ -20,3 +20,26 @@ class Vnf(object):
 
     def __getattr__(self, attr):
         return getattr(self.vnf_adapter, attr)
+
+    @log_entry_exit(LOG)
+    def config_applied(self, **credentials):
+        """
+        This function checks if the configuration has been applied to the VNF.
+
+        :param credentials: Packed dictionary containing the credentials used to login onto the VNF.
+        :return:            True if the configuration has been applied successfully, False otherwise.
+        """
+
+        LOG.debug('We are currently not checking if the configuration has been applied to the VNF')
+        return self.vnf_adapter.config_applied(**credentials)
+
+    def license_applied(self, **credentials):
+        """
+        This function checks if the license has been applied to the VNF.
+
+        :param credentials: Packed dictionary containing the credentials used to login onto the VNF.
+        :return:            True if the license has been applied successfully, False otherwise.
+        """
+
+        LOG.debug('We are currently not checking if the license has been applied to the VNF')
+        return self.vnf_adapter.license_applied(**credentials)
