1dc4dca4027e9154d08ac776dfd983779bb36808
Mihai Dragomir <mdragomir@luxoft.com>
2017-03-24 10:08:25 +0200

    Updated test case steps

--- test_cases/vnf/state/inst/TC_VNF_STATE_INST_001.py
+++ test_cases/vnf/state/inst/TC_VNF_STATE_INST_001.py
@@ -1,8 +1,9 @@
 import logging
-import yaml
 
 from api.generic import constants
 from test_cases import TestCase
+from api.generic.traffic import Traffic
+from api.generic.vnf import Vnf
 from api.generic.vnfm import Vnfm
 
 # Instantiate logger
@@ -11,30 +12,31 @@ LOG = logging.getLogger(__name__)
 
 class TC_VNF_STATE_INST_001(TestCase):
     """
-    TC_VNF_STATE_INST_001 VNF Instantiation without Element Management with configuration file without traffic
+    TC_VNF_STATE_INST_001 VNF Instantiation with configuration
 
     Sequence:
-    1. Instantiate the VNF without load
+    1. Instantiate the VNF
     2. Validate VNF instantiation state is INSTANTIATED and VNF state is STARTED
-    3. Update the VNF
-    4. Validate VNF instantiation state is INSTANTIATED and VNF state is STARTED
+    3. Validate configuration has been applied to the VNF
+    4. Validate license has been applied to the VNF (if applicable)
     5. Validate the right vResources have been allocated
-    6. Calculate the instantiation time
+    6. Start the low traffic load
+    7. Validate traffic flows
     """
 
     def setup(self):
         LOG.info('Starting setup for TC_VNF_STATE_INST_001')
 
         # Create objects needed by the test.
+        self.vnf = Vnf(vendor=self.tc_input['vnf']['type'])
         self.vnfm = Vnfm(vendor=self.tc_input['vnfm_params']['type'], **self.tc_input['vnfm_params']['client_config'])
+        self.traffic = Traffic(self.tc_input['traffic_params']['type'],
+                               **self.tc_input['traffic_params']['client_config'])
+        self.register_for_cleanup(self.traffic.destroy)
 
         # Initialize test case result.
         self.tc_result['overall_status'] = constants.TEST_PASSED
         self.tc_result['error_info'] = 'No errors'
-
-        # Store the VNF config.
-        with open(self.tc_input['vnf']['config'], 'r') as vnf_config_file:
-            self.vnf_config = yaml.load(vnf_config_file.read())
 
         LOG.info('Finished setup for TC_VNF_STATE_INST_001')
 
@@ -44,9 +46,9 @@ class TC_VNF_STATE_INST_001(TestCase):
         LOG.info('Starting TC_VNF_STATE_INST_001')
 
         # --------------------------------------------------------------------------------------------------------------
-        # 1. Instantiate the VNF without load
+        # 1. Instantiate the VNF
         # --------------------------------------------------------------------------------------------------------------
-        LOG.info('Instantiating the VNF without load')
+        LOG.info('Instantiating the VNF')
         self.time_record.START('instantiate_vnf')
         self.vnf_instance_id = self.vnfm.vnf_create_and_instantiate(
                                                                 vnfd_id=self.tc_input['vnfd_id'], flavour_id=None,
@@ -60,6 +62,8 @@ class TC_VNF_STATE_INST_001(TestCase):
             return False
 
         self.time_record.END('instantiate_vnf')
+
+        self.tc_result['durations']['instantiate_vnf'] = self.time_record.duration('instantiate_vnf')
 
         self.register_for_cleanup(self.vnfm.vnf_terminate_and_delete, vnf_instance_id=self.vnf_instance_id,
                                   termination_type='graceful')
@@ -87,40 +91,25 @@ class TC_VNF_STATE_INST_001(TestCase):
             return False
 
         # --------------------------------------------------------------------------------------------------------------
-        # 3. Update the VNF
+        # 3. Validate configuration has been applied to the VNF
         # --------------------------------------------------------------------------------------------------------------
-        LOG.info('Updating the VNF')
-        self.time_record.START('update_vnf')
-        if self.vnfm.modify_vnf_configuration_sync(self.vnf_instance_id, self.vnf_config,
-                                                   cooldown=constants.COLLDOWN) != constants.OPERATION_SUCCESS:
+        LOG.info('Validating configuration file has been applied to the VNF')
+        if not self.vnf.config_applied(**self.tc_input['vnf']['credentials']):
             LOG.error('TC_VNF_STATE_INST_001 execution failed')
-            LOG.debug('Could not update VNF')
+            LOG.debug('Configuration has not been applied to the VNF')
             self.tc_result['overall_status'] = constants.TEST_FAILED
-            self.tc_result['error_info'] = 'Could not update VNF'
+            self.tc_result['error_info'] = 'Configuration has not been applied to the VNF'
             return False
 
-        self.time_record.END('update_vnf')
-
         # --------------------------------------------------------------------------------------------------------------
-        # 4. Validate VNF instantiation state is INSTANTIATED and VNF state is STARTED
+        # 4. Validate license has been applied to the VNF (if applicable)
         # --------------------------------------------------------------------------------------------------------------
-        LOG.info('Validating VNF instantiation state is INSTANTIATED')
-        vnf_info = self.vnfm.vnf_query(filter={'vnf_instance_id': self.vnf_instance_id})
-        if vnf_info.instantiation_state != constants.VNF_INSTANTIATED:
+        LOG.info('Validating license has been applied to the VNF')
+        if not self.vnf.license_applied(**self.tc_input['vnf']['credentials']):
             LOG.error('TC_VNF_STATE_INST_001 execution failed')
-            LOG.debug('Unexpected VNF instantiation state')
+            LOG.debug('License has not been applied to the VNF')
             self.tc_result['overall_status'] = constants.TEST_FAILED
-            self.tc_result['error_info'] = 'VNF instantiation state was not "%s" after the VNF was instantiated' % \
-                                           constants.VNF_INSTANTIATED
-            return False
-
-        LOG.info('Validating VNF state is STARTED')
-        if vnf_info.instantiated_vnf_info.vnf_state != constants.VNF_STARTED:
-            LOG.error('TC_VNF_STATE_INST_001 execution failed')
-            LOG.debug('Unexpected VNF state')
-            self.tc_result['overall_status'] = constants.TEST_FAILED
-            self.tc_result['error_info'] = 'VNF state was not "%s" after the VNF was instantiated' % \
-                                           constants.VNF_STARTED
+            self.tc_result['error_info'] = 'License has not been applied to the VNF'
             return False
 
         # --------------------------------------------------------------------------------------------------------------
@@ -137,13 +126,44 @@ class TC_VNF_STATE_INST_001(TestCase):
         self.tc_result['resources']['Initial'] = self.vnfm.get_allocated_vresources(self.vnf_instance_id)
 
         # --------------------------------------------------------------------------------------------------------------
-        # 6. Calculate the instantiation time
+        # 6. Start the low traffic load
         # --------------------------------------------------------------------------------------------------------------
-        LOG.info('Calculating the instantiation time')
-        self.tc_result['durations']['instantiate_vnf'] = self.time_record.duration('instantiate_vnf')
-        self.tc_result['durations']['update_vnf'] = self.time_record.duration('update_vnf')
-        self.tc_result['durations']['instantiate_update_vnf'] = self.time_record.delta('instantiate_vnf.START',
-                                                                                       'update_vnf.END')
+        LOG.info('Starting the low traffic load')
+        if not self.traffic.configure(traffic_load='LOW_TRAFFIC_LOAD',
+                                      traffic_config=self.tc_input['traffic_params']['traffic_config']):
+            LOG.error('TC_VNF_STATE_INST_001 execution failed')
+            LOG.debug('Low traffic load and traffic configuration parameter could not be applied')
+            self.tc_result['overall_status'] = constants.TEST_FAILED
+            self.tc_result[
+                'error_info'] = 'Low traffic load and traffic configuration parameter could not be applied'
+            return False
+
+        if not self.traffic.start(return_when_emission_starts=True):
+            LOG.error('TC_VNF_STATE_INST_001 execution failed')
+            LOG.debug('Traffic could not be started')
+            self.tc_result['overall_status'] = constants.TEST_FAILED
+            self.tc_result['error_info'] = 'Low traffic could not be started'
+            return False
+
+        self.register_for_cleanup(self.traffic.stop)
+
+        # --------------------------------------------------------------------------------------------------------------
+        # 7. Validate traffic flows
+        # --------------------------------------------------------------------------------------------------------------
+        LOG.info('Validating traffic flows')
+        if not self.traffic.does_traffic_flow(delay_time=5):
+            LOG.error('TC_VNF_STATE_INST_001 execution failed')
+            LOG.debug('Traffic is not flowing')
+            self.tc_result['overall_status'] = constants.TEST_FAILED
+            self.tc_result['error_info'] = 'Low traffic did not flow'
+            return False
+
+        if self.traffic.any_traffic_loss():
+            LOG.error('TC_VNF_STATE_INST_001 execution failed')
+            LOG.debug('Traffic is flowing with packet loss')
+            self.tc_result['overall_status'] = constants.TEST_FAILED
+            self.tc_result['error_info'] = 'Low traffic flew with packet loss'
+            return False
 
         LOG.info('TC_VNF_STATE_INST_001 execution completed successfully')
 
