0db102e7ad881787b16e1829c33eb5f1d4e3dc56
Mihai Dragomir <mdragomir@luxoft.com>
2017-03-24 14:41:14 +0200

    Added python implementation for test case TC_VNF_STATE_INST_006

---
+++ test_cases/vnf/state/inst/TC_VNF_STATE_INST_006.py
@@ -0,0 +1,142 @@
+import logging
+
+from api.generic import constants
+from test_cases import TestCase
+from api.generic.traffic import Traffic
+from api.generic.vnfm import Vnfm
+
+# Instantiate logger
+LOG = logging.getLogger(__name__)
+
+
+class TC_VNF_STATE_INST_006(TestCase):
+    """
+    TC_VNF_STATE_INST_006 VNF Instantiation with inactive Element Management
+
+    Sequence:
+    1. Deactivate EM if needed
+    2. Instantiate the VNF
+    3. Validate VNF instantiation state is INSTANTIATED and VNF state is STARTED
+    4. Validate the right vResources have been allocated
+    5. Start the low traffic load
+    6. Validate no traffic flows
+    """
+
+    def setup(self):
+        LOG.info('Starting setup for TC_VNF_STATE_INST_006')
+
+        # Create objects needed by the test.
+        self.vnfm = Vnfm(vendor=self.tc_input['vnfm_params']['type'], **self.tc_input['vnfm_params']['client_config'])
+        self.traffic = Traffic(self.tc_input['traffic_params']['type'],
+                               **self.tc_input['traffic_params']['client_config'])
+        self.register_for_cleanup(self.traffic.destroy)
+
+        # Initialize test case result.
+        self.tc_result['overall_status'] = constants.TEST_PASSED
+        self.tc_result['error_info'] = 'No errors'
+
+        LOG.info('Finished setup for TC_VNF_STATE_INST_006')
+
+        return True
+
+    def run(self):
+        LOG.info('Starting TC_VNF_STATE_INST_006')
+
+        # --------------------------------------------------------------------------------------------------------------
+        # 1. Deactivate EM if needed
+        # --------------------------------------------------------------------------------------------------------------
+        LOG.info('Deactivating EM')
+        # TODO
+
+        # --------------------------------------------------------------------------------------------------------------
+        # 2. Instantiate the VNF
+        # --------------------------------------------------------------------------------------------------------------
+        LOG.info('Instantiating the VNF')
+        self.time_record.START('instantiate_vnf')
+        self.vnf_instance_id = self.vnfm.vnf_create_and_instantiate(
+                                                                vnfd_id=self.tc_input['vnfd_id'], flavour_id=None,
+                                                                vnf_instance_name=self.tc_input['vnf']['instance_name'],
+                                                                vnf_instance_description=None)
+        if self.vnf_instance_id is None:
+            LOG.error('TC_VNF_STATE_INST_006 execution failed')
+            LOG.debug('Unexpected VNF instantiation ID')
+            self.tc_result['overall_status'] = constants.TEST_FAILED
+            self.tc_result['error_info'] = 'VNF instantiation operation failed'
+            return False
+
+        self.time_record.END('instantiate_vnf')
+
+        self.register_for_cleanup(self.vnfm.vnf_terminate_and_delete, vnf_instance_id=self.vnf_instance_id,
+                                  termination_type='graceful')
+
+        # --------------------------------------------------------------------------------------------------------------
+        # 3. Validate VNF instantiation state is INSTANTIATED and VNF state is STARTED
+        # --------------------------------------------------------------------------------------------------------------
+        LOG.info('Validating VNF instantiation state is INSTANTIATED and VNF state is STARTED')
+        vnf_info = self.vnfm.vnf_query(filter={'vnf_instance_id': self.vnf_instance_id})
+        if vnf_info.instantiation_state != constants.VNF_INSTANTIATED:
+            LOG.error('TC_VNF_STATE_INST_006 execution failed')
+            LOG.debug('Unexpected VNF instantiation state')
+            self.tc_result['overall_status'] = constants.TEST_FAILED
+            self.tc_result['error_info'] = 'VNF instantiation state was not "%s" after the VNF was instantiated' % \
+                                           constants.VNF_INSTANTIATED
+            return False
+
+        LOG.info('Validating VNF state is STARTED')
+        if vnf_info.instantiated_vnf_info.vnf_state != constants.VNF_STARTED:
+            LOG.error('TC_VNF_STATE_INST_006 execution failed')
+            LOG.debug('Unexpected VNF state')
+            self.tc_result['overall_status'] = constants.TEST_FAILED
+            self.tc_result['error_info'] = 'VNF state was not "%s" after the VNF was instantiated' % \
+                                           constants.VNF_STARTED
+            return False
+
+        # --------------------------------------------------------------------------------------------------------------
+        # 4. Validate the right vResources have been allocated
+        # --------------------------------------------------------------------------------------------------------------
+        LOG.info('Validating the right vResources have been allocated')
+        if not self.vnfm.validate_allocated_vresources(self.tc_input['vnfd_id'], self.vnf_instance_id):
+            LOG.error('TC_VNF_STATE_INST_006 execution failed')
+            LOG.debug('Could not validate allocated vResources')
+            self.tc_result['overall_status'] = constants.TEST_FAILED
+            self.tc_result['error_info'] = 'Could not validate allocated vResources'
+            return False
+
+        self.tc_result['resources']['Initial'] = self.vnfm.get_allocated_vresources(self.vnf_instance_id)
+
+        # --------------------------------------------------------------------------------------------------------------
+        # 5. Start the low traffic load
+        # --------------------------------------------------------------------------------------------------------------
+        LOG.info('Starting the low traffic load')
+        if not self.traffic.configure(traffic_load='LOW_TRAFFIC_LOAD',
+                                      traffic_config=self.tc_input['traffic_params']['traffic_config']):
+            LOG.error('TC_VNF_STATE_INST_006 execution failed')
+            LOG.debug('Low traffic load and traffic configuration parameter could not be applied')
+            self.tc_result['overall_status'] = constants.TEST_FAILED
+            self.tc_result[
+                'error_info'] = 'Low traffic load and traffic configuration parameter could not be applied'
+            return False
+
+        if not self.traffic.start(return_when_emission_starts=True):
+            LOG.error('TC_VNF_STATE_INST_006 execution failed')
+            LOG.debug('Traffic could not be started')
+            self.tc_result['overall_status'] = constants.TEST_FAILED
+            self.tc_result['error_info'] = 'Low traffic could not be started'
+            return False
+
+        self.register_for_cleanup(self.traffic.stop)
+
+        # --------------------------------------------------------------------------------------------------------------
+        # 6. Validate no traffic flows
+        # --------------------------------------------------------------------------------------------------------------
+        LOG.info('Validating no traffic flows')
+        if self.traffic.does_traffic_flow(delay_time=5):
+            LOG.error('TC_VNF_STATE_INST_006 execution failed')
+            LOG.debug('Traffic is not flowing')
+            self.tc_result['overall_status'] = constants.TEST_FAILED
+            self.tc_result['error_info'] = 'Low traffic did not flow'
+            return False
+
+        LOG.info('TC_VNF_STATE_INST_006 execution completed successfully')
+
+        return True
