9955158a7184972f38ef2482f3275f2bda6c49aa
Mihai Dragomir <mdragomir@luxoft.com>
2017-04-26 14:03:08 +0300

    Updated the scaling steps to scale out the NS and not the VNF

--- test_cases/vnf/scale/out/TC_VNF_SCALE_OUT_001_5.py
+++ test_cases/vnf/scale/out/TC_VNF_SCALE_OUT_001_5.py
@@ -3,7 +3,7 @@ import logging
 from api.generic import constants
 from api.generic.mano import Mano
 from api.generic.traffic import Traffic
-from api.structures.objects import ScaleVnfData, ScaleByStepData
+from api.structures.objects import ScaleNsData, ScaleNsByStepsData
 from test_cases import TestCase
 
 # Instantiate logger
@@ -20,8 +20,8 @@ class TC_VNF_SCALE_OUT_001_5(TestCase):
     3. Validate VNF instantiation state is INSTANTIATED and VNF state is STARTED
     4. Start the low traffic load
     5. Validate the provided functionality and all traffic goes through
-    6. Trigger a resize of the VNF resources by instructing the MANO to scale out the VNF inside the NS
-    7. Validate VNF has resized by adding VNFCs
+    6. Trigger a resize of the NS resources by instructing the MANO to scale out the NS
+    7. Validate NS has resized by adding VNFs
     8. Determine if and length of service disruption
     9. Start the normal traffic load
     10. Validate increased capacity without loss
@@ -40,7 +40,7 @@ class TC_VNF_SCALE_OUT_001_5(TestCase):
         self.tc_result['overall_status'] = constants.TEST_PASSED
         self.tc_result['error_info'] = 'No errors'
         self.tc_result['events']['instantiate_ns'] = dict()
-        self.tc_result['events']['scale_out_vnf'] = dict()
+        self.tc_result['events']['scale_out_ns'] = dict()
         self.tc_result['events']['service_disruption'] = dict()
 
         LOG.info('Finished setup for TC_VNF_SCALE_OUT_001_5')
@@ -168,61 +168,58 @@ class TC_VNF_SCALE_OUT_001_5(TestCase):
             return False
 
         # --------------------------------------------------------------------------------------------------------------
-        # 6. Trigger a resize of the VNF resources by instructing the MANO to scale out the VNF inside the NS
+        # 6. Trigger a resize of the NS resources by instructing the MANO to scale out the NS
         # --------------------------------------------------------------------------------------------------------------
-        LOG.info('Triggering a resize of the VNF resources by instructing the MANO to scale out the VNF inside the NS')
+        LOG.info('Triggering a resize of the NS resources by instructing the MANO to scale out the NS')
 
-        # Build the ScaleVnfData information element
-        scale_vnf_data = ScaleVnfData()
-        scale_vnf_data.vnf_instance_id = self.vnf_instance_id
-        scale_vnf_data.type = 'scale_out'
-        scale_vnf_data.scale_by_step_data = ScaleByStepData()
-        scale_vnf_data.scale_by_step_data.type = 'scale_out'
-        scale_vnf_data.scale_by_step_data.aspect_id = self.tc_input['scaling']['aspect']
-        scale_vnf_data.scale_by_step_data.additional_param = [{
-            'scaling_policy_name': self.tc_input['scaling']['policies'][0]}]
+        # Build the ScaleNsData information element
+        scale_ns_data = ScaleNsData()
+        scale_ns_data.scale_ns_by_steps_data = ScaleNsByStepsData()
+        scale_ns_data.scale_ns_by_steps_data.scaling_direction = 'scale_out'
+        scale_ns_data.scale_ns_by_steps_data.aspect_id = self.tc_input['scaling']['aspect']
+        scale_ns_data.scale_ns_by_steps_data.number_of_steps = self.tc_input['scaling']['increment']
 
-        self.time_record.START('scale_out_vnf')
-        if self.mano.ns_scale_sync(self.ns_instance_id, scale_type='scale_vnf', scale_vnf_data=scale_vnf_data) \
+        self.time_record.START('scale_out_ns')
+        if self.mano.ns_scale_sync(self.ns_instance_id, scale_type='scale_ns', scale_ns_data=scale_ns_data) \
                 != constants.OPERATION_SUCCESS:
             LOG.error('TC_VNF_SCALE_OUT_001_5 execution failed')
-            LOG.debug('MANO could not scale out the VNF')
+            LOG.debug('MANO could not scale out the NS')
             self.tc_result['overall_status'] = constants.TEST_FAILED
-            self.tc_result['error_info'] = 'MANO could not scale out the VNF'
+            self.tc_result['error_info'] = 'MANO could not scale out the NS'
             return False
 
-        self.time_record.END('scale_out_vnf')
+        self.time_record.END('scale_out_ns')
 
-        self.tc_result['events']['scale_out_vnf']['duration'] = self.time_record.duration('scale_out_vnf')
+        self.tc_result['events']['scale_out_ns']['duration'] = self.time_record.duration('scale_out_ns')
 
         self.tc_result['resources']['After scale out'] = self.mano.get_allocated_vresources(self.vnf_instance_id)
 
         self.tc_result['scaling_out']['status'] = 'Success'
 
         # --------------------------------------------------------------------------------------------------------------
-        # 6. Validate VNF has resized by adding VNFCs
+        # 7. Validate NS has resized by adding VNFs
         # --------------------------------------------------------------------------------------------------------------
-        LOG.info('Validating VNF has resized by adding VNFCs')
-        vnf_info = self.mano.vnf_query(filter={'vnf_instance_id': self.vnf_instance_id})
-        if len(vnf_info.instantiated_vnf_info.vnfc_resource_info) != self.tc_input['scaling']['default_instances'] + \
+        LOG.info('Validating NS has resized by adding VNFs')
+        ns_info = self.mano.ns_query(filter={'ns_instance_id': self.ns_instance_id})
+        if len(ns_info.vnf_info_id) != self.tc_input['scaling']['default_instances'] + \
                 self.tc_input['scaling']['increment']:
             LOG.error('TC_VNF_SCALE_OUT_001_5 execution failed')
-            LOG.debug('VNFCs not added after VNF scaled out')
+            LOG.debug('VNFs not added after NS scaled out')
             self.tc_result['overall_status'] = constants.TEST_FAILED
-            self.tc_result['error_info'] = 'VNFCs not added after VNF scaled out'
+            self.tc_result['error_info'] = 'VNFs not added after NS scaled out'
             return False
 
         self.tc_result['scaling_out']['level'] = self.tc_input['scaling']['default_instances'] + \
                                                  self.tc_input['scaling']['increment']
 
         # --------------------------------------------------------------------------------------------------------------
-        # 7. Determine if and length of service disruption
+        # 8. Determine if and length of service disruption
         # --------------------------------------------------------------------------------------------------------------
         LOG.info('Determining if and length of service disruption')
         self.tc_result['events']['service_disruption']['duration'] = self.traffic.calculate_service_disruption_length()
 
         # --------------------------------------------------------------------------------------------------------------
-        # 8. Start the normal traffic load
+        # 9. Start the normal traffic load
         # --------------------------------------------------------------------------------------------------------------
         LOG.info('Starting the normal traffic load')
 
@@ -236,9 +233,19 @@ class TC_VNF_SCALE_OUT_001_5(TestCase):
 
         # Configure stream destination MAC address(es).
         dest_mac_addr_list = ''
-        for ext_cp_info in vnf_info.instantiated_vnf_info.ext_cp_info:
-            if ext_cp_info.cpd_id == self.tc_input['traffic_params']['traffic_config']['left_cp_name']:
-                dest_mac_addr_list += ext_cp_info.address[0] + ' '
+
+        for index in range(0, len(ns_info.vnf_info_id)):
+
+            # Get the instance ID of the VNF inside the NS for the current index
+            vnf_instance_id = ns_info.vnf_info_id[index]
+            print vnf_instance_id
+
+            # Get the VnfInfo information element for the VNF with the current vnf_instance_id
+            vnf_info = self.mano.vnf_query(filter={'vnf_instance_id': vnf_instance_id})
+
+            for ext_cp_info in vnf_info.instantiated_vnf_info.ext_cp_info:
+                if ext_cp_info.cpd_id == self.tc_input['traffic_params']['traffic_config']['left_cp_name']:
+                    dest_mac_addr_list += ext_cp_info.address[0] + ' '
 
         self.traffic.config_traffic_stream(dest_mac_addr_list)
         self.traffic.config_traffic_load('NORMAL_TRAFFIC_LOAD')
@@ -252,7 +259,7 @@ class TC_VNF_SCALE_OUT_001_5(TestCase):
             return False
 
         # --------------------------------------------------------------------------------------------------------------
-        # 9. Validate increased capacity without loss
+        # 10. Validate increased capacity without loss
         # --------------------------------------------------------------------------------------------------------------
         LOG.info('Validating increased capacity without loss')
         if not self.traffic.does_traffic_flow(delay_time=5):
