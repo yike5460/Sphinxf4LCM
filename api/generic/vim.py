3e9f128272ff17edba96ded8aa3676c0020385e6
Catalin Udroiu <cudroiu@luxoft.com>
2017-05-08 10:29:43 +0300

    Added generic VIM functions to retrieve quotas for Compute, Network and Storage.

--- api/generic/vim.py
+++ api/generic/vim.py
@@ -20,6 +20,57 @@ class Vim(object):
         self.vim_adapter = construct_adapter(vendor, module_type='vim', **kwargs)
 
     @log_entry_exit(LOG)
+    def query_compute_resource_quota(self, query_compute_quota_filter=None):
+        """
+        This function queries the VIM to get information about compute resources that the consumer has access to.
+
+        This function was written in accordance with section 7.9.1.3 of ETSI GS NFV-IFA 005 v2.1.1 (2016-04).
+
+        :param query_compute_quota_filter:  Query filter based on e.g. name, identifier, meta-data information or status
+                                            information, expressing the type of information to be retrieved. It can also
+                                            be used to specify one or more resources to be queried by providing their
+                                            identifiers.
+
+        :return:                            Element containing information about the quota resource. The cardinality can
+                                            be 0 if no matching quota exists.
+        """
+        return self.vim_adapter.query_compute_resource_quota(query_compute_quota_filter)
+
+    @log_entry_exit(LOG)
+    def query_network_resource_quota(self, query_network_quota_filter=None):
+        """
+        This function queries the VIM to get information about network resources that the consumer has access to.
+
+        This function was written in accordance with section 7.9.2.3 of ETSI GS NFV-IFA 005 v2.1.1 (2016-04).
+
+        :param query_network_quota_filter:  Query filter based on e.g. name, identifier, meta-data information or status
+                                            information, expressing the type of information to be retrieved. It can also
+                                            be used to specify one or more resources to be queried by providing their
+                                            identifiers.
+
+        :return:                            Element containing information about the quota resource. The cardinality can
+                                            be 0 if no matching quota exists.
+        """
+        return self.vim_adapter.query_network_resource_quota(query_network_quota_filter)
+
+    @log_entry_exit(LOG)
+    def query_storage_resource_quota(self, query_storage_quota_filter=None):
+        """
+        This function queries the VIM to get information about storage resources that the consumer has access to.
+
+        This function was written in accordance with section 7.9.3.3 of ETSI GS NFV-IFA 005 v2.1.1 (2016-04).
+
+        :param query_storage_quota_filter:  Query filter based on e.g. name, identifier, meta-data information or status
+                                            information, expressing the type of information to be retrieved. It can also
+                                            be used to specify one or more resources to be queried by providing their
+                                            identifiers.
+
+        :return:                            Element containing information about the quota resource. The cardinality can
+                                            be 0 if no matching quota exists.
+        """
+        return self.vim_adapter.query_storage_resource_quota(query_storage_quota_filter)
+
+    @log_entry_exit(LOG)
     def query_virtualised_compute_resource(self, query_compute_filter):
         """
         This function allows querying information about instantiated virtualised compute resources.
