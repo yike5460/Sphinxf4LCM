7a9acaa30918dc33951dc7f1ac0c3803e14801e9
Alex Carp <acarp@luxoft.com>
2017-05-18 19:08:01 +0300

    Added thread safe tee based on itertools.tee Python implementation

--- utils/misc.py
+++ utils/misc.py
@@ -1,4 +1,7 @@
+import collections
 import datetime
+
+from threading import Lock
 
 
 def generate_name(name):
@@ -8,3 +11,26 @@ def generate_name(name):
 
     new_name = str(name) + '_{:%Y_%m_%d_%H_%M_%S}'.format(datetime.datetime.now())
     return new_name
+
+
+def tee(iterable, n=2):
+    """
+    This function is based on the Python implementation of itertools.tee:
+    https://docs.python.org/2/library/itertools.html#itertools.tee
+
+    A lock was added for making it safe to use the teed iterators in separate threads
+    """
+    it = iter(iterable)
+    l = Lock()
+    deques = [collections.deque() for i in range(n)]
+
+    def gen(mydeque):
+        while True:
+            if not mydeque:  # when the local deque is empty
+                with l:
+                    newval = next(it)  # fetch a new value and
+                for d in deques:  # load it to all the deques
+                    d.append(newval)
+            yield mydeque.popleft()
+
+    return tuple(gen(d) for d in deques)
