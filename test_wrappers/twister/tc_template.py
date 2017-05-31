d9c62b1412e278cabb5c8d683e9dd4bdf2610ac2
Mihai Dragomir <mdragomir@luxoft.com>
2017-05-31 16:24:17 +0300

    Updated twister tc_template

--- test_wrappers/twister/tc_template.py
+++ test_wrappers/twister/tc_template.py
@@ -18,7 +18,8 @@ def twister_run(tc_module_name, tc_name):
     # Logger for the current module.
     LOG = logging.getLogger('top_level_script')
 
-    cfg_file = get_config(CONFIG[0], 'cfg_path/path')
+    cfg_file_path = get_config(CONFIG[0], 'config/tc_input_dir')
+    cfg_file = cfg_file_path + '/' + tc_name + '.json'
     with open(cfg_file) as f:
         tc_input = json.load(f)
 
