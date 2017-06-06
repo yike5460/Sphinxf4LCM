15160414a4165fa3960cd6d42682198ebe7f6801
Mihai Dragomir <mdragomir@luxoft.com>
2017-06-06 11:58:01 +0300

    Added Cirros VNF adapter

---
+++ api/adapter/vnf/cirros.py
@@ -0,0 +1,18 @@
+import logging
+
+from utils.logging_module import log_entry_exit
+
+LOG = logging.getLogger(__name__)
+
+
+class CirrosVnfAdapter(object):
+    def __init__(self):
+        pass
+
+    @log_entry_exit(LOG)
+    def config_applied(self, **credentials):
+        return True
+
+    @log_entry_exit(LOG)
+    def license_applied(self, **credentials):
+        return True
