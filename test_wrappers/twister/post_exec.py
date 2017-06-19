64bb6879d3f9df5ea684b92216b880186ad6f23a
Mihai Dragomir <mdragomir@luxoft.com>
2017-06-19 15:55:38 +0300

    Added twister post execution script

---
+++ test_wrappers/twister/post_exec.py
@@ -0,0 +1,16 @@
+import requests
+
+# Get VNF Lifecycle Verification server IP address
+vnf_lcv_srv = '10.2.34.39'
+
+# Get the list with the test cases under execution
+server_response = requests.get(url='http://' + vnf_lcv_srv + ':8080/v1.0/exec')
+execution_list = server_response.json()
+
+# Stop all 'PENDING' executions
+for execution_entry in execution_list['status_list']:
+    execution_id = execution_entry['execution_id']
+    server_response = requests.get(url='http://' + vnf_lcv_srv + ':8080/v1.0/exec/' + execution_id)
+    data = server_response.json()
+    if data['status'] == 'PENDING':
+        requests.delete(url='http://' + vnf_lcv_srv + ':8080/v1.0/exec/' + execution_id)
