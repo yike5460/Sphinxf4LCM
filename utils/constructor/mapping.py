1f301790748982061aa102e2a93673297ca30dc5
Catalin Udroiu <cudroiu@luxoft.com>
2017-10-16 16:51:54 +0300

    Optimized code for utils/constructor/mapping.py

--- utils/constructor/mapping.py
+++ utils/constructor/mapping.py
@@ -4,6 +4,7 @@ from api import ApiError
 
 adapter_mapping_file_name = 'adapter_construct.json'
 generic_mapping_file_name = 'generic_construct.json'
+_constructor_mapping = {'adapter': None, 'generic': None}
 
 class ConstructorError(ApiError):
     """
@@ -15,17 +16,15 @@ def get_constructor_mapping(type):
     """
     This function returns a dictionary representing the mappings between various generic modules and their API path.
     """
+    global _constructor_mapping
     if type == 'adapter':
         mapping_file_name = adapter_mapping_file_name
-        _constructor_mapping = None
     elif type == 'generic':
         mapping_file_name = generic_mapping_file_name
-        _constructor_mapping = None
     else:
         raise ConstructorError('Unable to load mapping file for constructor of type %s' % type)
-    # global _constructor_mapping
-    if _constructor_mapping is None:
+    if _constructor_mapping[type] is None:
         mapping_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), mapping_file_name)
         with open(mapping_file_path, 'r') as mapping_file:
-            _constructor_mapping = json.load(mapping_file)
-    return _constructor_mapping
+            _constructor_mapping[type] = json.load(mapping_file)
+    return _constructor_mapping[type]
