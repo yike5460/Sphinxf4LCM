67f20f30ee3e43591c56da7d69e99f7bc31829da
Catalin Udroiu <cudroiu@luxoft.com>
2017-12-13 16:20:28 +0200

    Submitting first version for TD_NFV_NSLCM_SCALE_IN_VNF_001 test case.

--- test_cases/tst_007/ns/scale/in/TD_NFV_NSLCM_SCALE_IN_VNF_001.py
+++ test_cases/tst_007/ns/scale/in/TD_NFV_NSLCM_SCALE_IN_VNF_001.py
@@ -4,6 +4,7 @@ from api.generic import constants
 from api.structures.objects import ScaleVnfData, ScaleByStepData
 from test_cases import TestCase, TestRunError
 from utils.misc import generate_name
+from utils.net import ping
 
 # Instantiate logger
 LOG = logging.getLogger(__name__)
@@ -108,11 +109,7 @@ class TD_NFV_NSLCM_SCALE_IN_VNF_001(TestCase):
 
             scale_vnf_data_list.append(scale_vnf_data)
 
-            LOG.info('### VNF %s - Default number of instances: %s' % (vnf_name, sp['default_instances']))
-            LOG.info('### VNF %s - Increment number of instances: %s' % (vnf_name, sp['increment']))
             expected_nr_vnfc[vnf_name] = sp['default_instances'] + sp['increment']
-            LOG.info('### After scale out:')
-            LOG.info('### VNF %s - Expected number of instances: %s' % (vnf_name, expected_nr_vnfc[vnf_name]))
 
         self.time_record.START('scale_out_ns')
         if self.mano.ns_scale_sync(self.ns_instance_id, scale_type='SCALE_VNF', scale_vnf_data=scale_vnf_data_list,
@@ -138,30 +135,11 @@ class TD_NFV_NSLCM_SCALE_IN_VNF_001(TestCase):
         # --------------------------------------------------------------------------------------------------------------
         LOG.info('Triggering NS scale in by removing VNFC instance(s) from a VNF in the NS in NFVO with an operator'
                  ' action')
-        # scale_vnf_data_list = list()
-        # expected_nr_vnfc = dict()
         for scale_vnf_data in scale_vnf_data_list:
-            # vnf_name, sp_name = vnf_sp.split(':')
-            # vnfd_name = self.mano.get_vnfd_name_from_nsd_vnf_name(self.tc_input['nsd_id'], vnf_name)
-            # sp = self.mano.get_vnfd_scaling_properties(vnfd_name, sp_name)
-            #
-            # # Build the ScaleVnfData information element
-            # scale_vnf_data = ScaleVnfData()
-            # scale_vnf_data.vnf_instance_id = self.mano.get_vnf_instance_id_from_ns_vnf_name(ns_info, vnf_name)
             scale_vnf_data.type = 'in'
-            # scale_vnf_data.scale_by_step_data = ScaleByStepData()
             scale_vnf_data.scale_by_step_data.type = 'in'
-            # scale_vnf_data.scale_by_step_data.aspect_id = sp['targets'][0]
-            # scale_vnf_data.scale_by_step_data.number_of_steps = sp['increment']
-            # scale_vnf_data.scale_by_step_data.additional_param = {'scaling_policy_name': sp_name}
-            #
-            # scale_vnf_data_list.append(scale_vnf_data)
 
-            # LOG.info('### Before scale in:')
-            # LOG.info('### VNF %s - Expected number of instances: %s' % (vnf_name, expected_nr_vnfc[vnf_name]))
-            # expected_nr_vnfc[vnf_name] = expected_nr_vnfc[vnf_name] - scale_vnf_data.scale_by_step_data.number_of_steps
-            # LOG.info('### After scale in:')
-            # LOG.info('### VNF %s - Expected number of instances: %s' % (vnf_name, expected_nr_vnfc[vnf_name]))
+            expected_nr_vnfc[vnf_name] = expected_nr_vnfc[vnf_name] - scale_vnf_data.scale_by_step_data.number_of_steps
 
         self.time_record.START('scale_in_ns')
         if self.mano.ns_scale_sync(self.ns_instance_id, scale_type='SCALE_VNF', scale_vnf_data=scale_vnf_data_list,
@@ -184,8 +162,6 @@ class TD_NFV_NSLCM_SCALE_IN_VNF_001(TestCase):
         for vnf_info in ns_info.vnf_info:
             vnf_name = vnf_info.vnf_product_name
             if vnf_name in expected_nr_vnfc.keys():
-                # LOG.info('### VNF %s - Actual number of instances after scale out and in: %s' %
-                #          (vnf_name, len(vnf_info.instantiated_vnf_info.vnfc_resource_info)))
                 if len(vnf_info.instantiated_vnf_info.vnfc_resource_info) != expected_nr_vnfc[vnf_name]:
                     raise TestRunError('VNFCs not removed after VNF scaled in')
 
@@ -203,8 +179,9 @@ class TD_NFV_NSLCM_SCALE_IN_VNF_001(TestCase):
         # --------------------------------------------------------------------------------------------------------------
         LOG.info('Verifying that the impacted VNFC instance(s) resources have been released by the VIM')
         for vnf_info_impacted in vnf_info_impacted_list:
-            vnf_info_final = self.mano.vnf_query(filter={'vnf_instance_id': vnf_info_impacted.vnf_instance_id,
-                                                         'additional_param': self.tc_input['mano'].get('query_params')})
+            for vnf_info in ns_info.vnf_info:
+                if vnf_info_impacted.vnf_instance_id == vnf_info.vnf_instance_id:
+                    vnf_info_final = vnf_info
             if not self.mano.validate_vnf_released_vresources(vnf_info_initial=vnf_info_impacted,
                                                               vnf_info_final=vnf_info_final):
                 raise TestRunError('Allocated vResources were not released for VNF instance ID %s' %
@@ -213,26 +190,67 @@ class TD_NFV_NSLCM_SCALE_IN_VNF_001(TestCase):
         # --------------------------------------------------------------------------------------------------------------
         # 7.  Verify that the remaining VNFC instance(s) are still running and reachable via the management network
         # --------------------------------------------------------------------------------------------------------------
-
+        LOG.info('Verifying that the remaining VNFC instance(s) are still running and reachable via the management'
+                 ' network')
+        for vnf_info in ns_info.vnf_info:
+            mgmt_addr_list = self.mano.get_vnf_mgmt_addr_list(vnf_info.vnf_instance_id)
+            print mgmt_addr_list
+            for mgmt_addr in mgmt_addr_list:
+                if not ping(mgmt_addr):
+                    raise TestRunError('Unable to PING IP address %s belonging to %s'
+                                       % (mgmt_addr, vnf_info.vnf_product_name))
 
         # --------------------------------------------------------------------------------------------------------------
         # 8.  Verify that the VNF configuration has been updated to exclude the removed VNFC instances according to the
         # descriptors by querying the VNFM
         # --------------------------------------------------------------------------------------------------------------
-
+        LOG.info('Verify that the VNF configuration has been updated to exclude the additional VNFC instances according'
+                 ' to the descriptors by querying the VNFM')
+        # TODO
 
         # --------------------------------------------------------------------------------------------------------------
         # 9.  Verify that the remaining VNFC instances(s) and VL(s) are still connected according to the descriptors
         # --------------------------------------------------------------------------------------------------------------
-
+        LOG.info('Verifying that the remaining VNFC instances(s) and VL(s) are still connected according to the '
+                 'descriptors')
+        # TODO
 
         # --------------------------------------------------------------------------------------------------------------
         # 10.  Verify that the NFVO indicates the scaling operation result as successful
         # --------------------------------------------------------------------------------------------------------------
-
+        LOG.info('Verifying that the NFVO indicates the scaling operation result as successful')
+        LOG.debug('This has implicitly been checked at step 4')
 
         # --------------------------------------------------------------------------------------------------------------
         # 11. Verify that NS has been scaled in by running the end-to-end functional test in relevance to the VNF scale
         # and capacity
         # --------------------------------------------------------------------------------------------------------------
+        LOG.info('Verify that NS has been scaled in by running the end-to-end functional test in relevance to the VNF '
+                 'scale and capacity')
+        self.traffic.configure(traffic_load='NORMAL_TRAFFIC_LOAD',
+                               traffic_config=self.tc_input['traffic']['traffic_config'])
+
+        self.register_for_cleanup(index=30, function_reference=self.traffic.destroy)
+
+        # Configure stream destination address(es)
+        dest_addr_list = self.mano.get_ns_ingress_cp_addr_list(
+                                                          ns_info,
+                                                          self.tc_input['traffic']['traffic_config']['ingress_cp_name'])
+        self.traffic.reconfig_traffic_dest(dest_addr_list)
+
+        self.traffic.start(return_when_emission_starts=True)
+
+        self.register_for_cleanup(index=40, function_reference=self.traffic.stop)
+
+        # Letting the traffic flow for a bit to make sure the network has converged
+        if not self.traffic.does_traffic_flow(delay_time=60):
+            raise TestRunError('Traffic is not flowing', err_details='Normal traffic did not flow')
+
+        # Clearing counters so that any packets lost so far don't influence the results
+        self.traffic.clear_counters()
+
+        if self.traffic.any_traffic_loss(delay_time=5, tolerance=constants.TRAFFIC_TOLERANCE):
+            raise TestRunError('Traffic is flowing with packet loss',
+                               err_details='Normal traffic flew with packet loss')
+
         LOG.info('%s execution completed successfully' % self.tc_name)
