93d7e27d433598aee24c463665d79e0130442e64
Alex Carp <acarp@luxoft.com>
2018-01-17 15:12:21 +0100

    #128: Add mac_address in ext_cp_info

--- api/adapter/mano/cisconfv45.py
+++ api/adapter/mano/cisconfv45.py
@@ -451,10 +451,22 @@ class CiscoNFVManoAdapter(object):
                                                          % (vm_group_text, vm_id_text, nic_id_text))
                         ip_address_text = ip_address.text
 
+                        # Get the IP address
+                        mac_address = deployment_xml.find('.//{http://www.cisco.com/esc/esc}vm_group'
+                                                         '[{http://www.cisco.com/esc/esc}name="%s"]/'
+                                                         '{http://www.cisco.com/esc/esc}vm_instance'
+                                                         '[{http://www.cisco.com/esc/esc}vm_id="%s"]/'
+                                                         '{http://www.cisco.com/esc/esc}interfaces/'
+                                                         '{http://www.cisco.com/esc/esc}interface'
+                                                         '[{http://www.cisco.com/esc/esc}nicid="%s"]/'
+                                                         '{http://www.cisco.com/esc/esc}mac_address'
+                                                         % (vm_group_text, vm_id_text, nic_id_text))
+                        mac_address_text = mac_address.text
+
                         # Build the VnfExtCpInfo data structure
                         vnf_ext_cp_info = VnfExtCpInfo()
                         vnf_ext_cp_info.cp_instance_id = port_id_text
-                        vnf_ext_cp_info.address = [ip_address_text]
+                        vnf_ext_cp_info.address = [mac_address_text]
                         vnf_ext_cp_info.cpd_id = cpd_id_text
 
                         # Append the current VnfExtCpInfo element to the VnfExtCpInfo list
