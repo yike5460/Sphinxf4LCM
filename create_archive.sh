2d65ff7e4619ce81f07edc0bdabaea9a02de4779
Alex Carp <acarp@luxoft.com>
2018-03-01 17:02:42 +0200

    #93: Added intellectual property banner to files in /

--- create_archive.sh
+++ create_archive.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+#
+# Copyright (c) 2018 by Spirent Communications Plc.
+# All Rights Reserved.
+#
+# This software is confidential and proprietary to Spirent Communications Inc.
+# No part of this software may be reproduced, transmitted, disclosed or used
+# in violation of the Software License Agreement without the expressed
+# written consent of Spirent Communications Inc.
+#
+#
+
+
 #
 # Script for creating VNF Lifecycle Validation deployment archive 
 # version 1.0: 13.11.2017
