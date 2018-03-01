6c8553ce627c3212bc32e09c76a2a8cd6465ecce
Alex Carp <acarp@luxoft.com>
2018-03-01 17:01:02 +0200

    #93: Added intellectual property banner to files in test_wrappers/

--- test_wrappers/twister/post_exec.py
+++ test_wrappers/twister/post_exec.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+#
+# Copyright (c) 2018 by Spirent Communications Plc.
+# All Rights Reserved.
+#
+# This software is confidential and proprietary to Spirent Communications Inc.
+# No part of this software may be reproduced, transmitted, disclosed or used
+# in violation of the Software License Agreement without the expressed
+# written consent of Spirent Communications Inc.
+#
+#
+
+
 import requests
 
 # TODO: Get VNF Lifecycle Verification server IP address
