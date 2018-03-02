c2e59229dafbb9ae43fff967305a58e619d0b8bf
Mihai Dragomir <mdragomir@luxoft.com>
2018-03-02 15:27:02 +0200

    Cosmetic updates

--- test_cases/vnf/scale/out/TC_VNFC_SCALE_OUT_005__EM_MANUAL.py
+++ test_cases/vnf/scale/out/TC_VNFC_SCALE_OUT_005__EM_MANUAL.py
@@ -151,7 +151,7 @@ class TC_VNFC_SCALE_OUT_005__EM_MANUAL(TestCase):
         LOG.info('Triggering a resize of the VNF resources to use more specialized hardware by instructing the EM to '
                  'instruct the MANO to scale out the VNF')
         self.time_record.START('scale_out_vnf')
-        if self.em.vnf_scale_sync(self.vnf_instance_id, scale_type='out',aspect_id=sp['targets'],
+        if self.em.vnf_scale_sync(self.vnf_instance_id, scale_type='out', aspect_id=sp['targets'],
                                   additional_param={'scaling_policy_name': self.tc_input['scaling_policy_name']}) \
                 != constants.OPERATION_SUCCESS:
             self.tc_result['scaling_out']['status'] = 'Fail'
