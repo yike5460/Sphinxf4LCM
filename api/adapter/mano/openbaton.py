b2c984eaeaf6cc128282e2401605c2d2243030b8
Catalin Udroiu <cudroiu@luxoft.com>
2018-03-16 16:39:27 +0200

    Added new functions for Openbaton MANO adapter. This is to support TD_NFV_NSLCM_INSTANTIATE_001 test case.

--- api/adapter/mano/openbaton.py
+++ api/adapter/mano/openbaton.py
@@ -11,7 +11,6 @@
 
 
 import logging
-import re
 import time
 
 import requests
@@ -67,7 +66,8 @@ class OpenbatonManoAdapter(object):
                                  data=body, headers = http_headers, verify=False)
         try:
             assert response.status_code == 200
-        except Exception:
+        except Exception as e:
+            LOG.debug(e)
             raise OpenbatonManoAdapterError('Unable to fetch Authorization token from %s' %
                                             self.api_url + '/oauth/token')
         token = str('Bearer ' + response.json()['access_token'])
@@ -157,10 +157,10 @@ class OpenbatonManoAdapter(object):
                 for vnfr in ns_config.get('vnfr'):
                     self.vnf_to_ns_mapping[str(vnfr.get('id'))] = resource_id
                 return constants.OPERATION_SUCCESS
-            elif ns_status == 'NULL':
-                return constants.OPERATION_PENDING
             elif ns_status == 'ERROR':
                 return constants.OPERATION_FAILED
+            else:
+                return constants.OPERATION_PENDING
 
         if operation_type == 'ns_terminate':
             url = '/api/v1/ns-records/%s' % resource_id
@@ -226,7 +226,7 @@ class OpenbatonManoAdapter(object):
                 for vnfc_instance in vdu.get('vnfc_instance'):
                     vnfc_resource_info = VnfcResourceInfo()
                     vnfc_resource_info.vnfc_instance_id = str(vnfc_instance.get('id'))
-                    vnfc_resource_info.vdu_id = str(vdu.get('id'))
+                    vnfc_resource_info.vdu_id = str(vdu.get('parent_vdu'))
                     vnfc_resource_info.compute_resource = ResourceHandle()
                     vnfc_resource_info.compute_resource.vim_id = str(vnfc_instance.get('vim_id'))
                     vnfc_resource_info.compute_resource.resource_id = str(vnfc_instance.get('vc_id'))
@@ -234,14 +234,14 @@ class OpenbatonManoAdapter(object):
                     vnf_info.instantiated_vnf_info.ext_cp_info = list()
                     for ext_cp in vnfc_instance.get('vnfComponent').get('connection_point'):
                         vnf_ext_cp_info = VnfExtCpInfo()
-                        vnf_ext_cp_info.cp_instance_id = str(ext_cp.get('id'))
                         vnf_ext_cp_info.cpd_id = str(ext_cp.get('virtual_link_reference_id'))
-                        # virtual_link_reference = str(ext_cp['virtual_link_reference'])
-                        vnf_ext_cp_info.address = list()
-                        # for ip in vnfc_instance['ips']:
-                        #     if ip['netName'] == virtual_link_reference:
-                        #         vnf_ext_cp_info.address.append(str(ip['ip']))
-                        #         break
+                        virtual_link_reference = ext_cp.get('virtual_link_reference')
+                        address_list = list()
+                        for ip in vnfc_instance['ips']:
+                            if ip['netName'] == virtual_link_reference:
+                                address_list.append(str(ip['ip']))
+                                break
+                        vnf_ext_cp_info.address = address_list
                         vnf_info.instantiated_vnf_info.ext_cp_info.append(vnf_ext_cp_info)
             ns_info.vnf_info.append(vnf_info)
         return ns_info
@@ -264,8 +264,6 @@ class OpenbatonManoAdapter(object):
             LOG.exception(e)
             raise OpenbatonManoAdapterError('Unable to retrieve status for VNF with ID %s. Reason: %s' %
                                             (vnf_instance_id, e.message))
-        print resp.status_code
-        print vnf_config
         vnf_info.vnf_instance_id = str(vnf_config.get('id'))
         if vnf_config.get('status') == 'ACTIVE':
             vnf_info.instantiation_state = constants.VNF_INSTANTIATED
@@ -295,12 +293,13 @@ class OpenbatonManoAdapter(object):
                     vnf_ext_cp_info = VnfExtCpInfo()
                     vnf_ext_cp_info.cp_instance_id = str(ext_cp.get('id'))
                     vnf_ext_cp_info.cpd_id = str(ext_cp.get('virtual_link_reference_id'))
-                    # virtual_link_reference = str(ext_cp['virtual_link_reference'])
-                    vnf_ext_cp_info.address = list()
-                    # for ip in vnfc_instance['ips']:
-                    #     if ip['netName'] == virtual_link_reference:
-                    #         vnf_ext_cp_info.address.append(str(ip['ip']))
-                    #         break
+                    virtual_link_reference = ext_cp.get('virtual_link_reference')
+                    address_list = list()
+                    for ip in vnfc_instance['ips']:
+                        if ip['netName'] == virtual_link_reference:
+                            address_list.append(str(ip['ip']))
+                            break
+                    vnf_ext_cp_info.address = address_list
                     vnf_info.instantiated_vnf_info.ext_cp_info.append(vnf_ext_cp_info)
         return vnf_info
 
@@ -378,3 +377,133 @@ class OpenbatonManoAdapter(object):
         LOG.debug('NS with ID %s did not reach a stable state after %s' % (ns_instance_id, max_wait_time))
         return False
 
+    @log_entry_exit(LOG)
+    def get_vnfd(self, vnfd_id):
+        url = '/api/v1/vnf-descriptors/%s' % vnfd_id
+        method = 'get'
+        try:
+            resp, vnfd = self.do_request(url=url, method=method)
+            assert resp.status_code == 200
+        except Exception as e:
+            LOG.exception(e)
+            raise OpenbatonManoAdapterError('Unable to retrieve config for VNFD with ID %s. Reason: %s' % (vnfd_id, e.message))
+        return vnfd
+
+    @log_entry_exit(LOG)
+    def verify_vnf_sw_images(self, vnf_info, additional_param=None):
+        vnfd_id = vnf_info.vnfd_id
+        vnfd = self.get_vnfd(vnfd_id)
+
+        expected_vdu_images = {}
+        for vdu in vnfd.get('vdu'):
+            expected_vdu_images[vdu.get('id')] = vdu.get('vm_image')
+
+        for vnfc_resource_info in vnf_info.instantiated_vnf_info.vnfc_resource_info:
+            vdu_id = vnfc_resource_info.vdu_id
+
+            vim_id = vnfc_resource_info.compute_resource.vim_id
+            vim = self.get_vim_helper(vim_id)
+            resource_id = vnfc_resource_info.compute_resource.resource_id
+            virtual_compute = vim.query_virtualised_compute_resource(filter={'compute_id': resource_id})
+            image_id = virtual_compute.vc_image_id
+            image_details = vim.query_image(image_id)
+            image_name_vim = image_details.name
+
+            image_name_vnfd = expected_vdu_images[vdu_id]
+            if image_name_vim not in image_name_vnfd:
+                LOG.debug('Unexpected image for VNFC %s, VDU type %s' % (resource_id, vdu_id))
+                LOG.debug('Expected image name: %s; actual image name: %s' % (image_name_vnfd, image_name_vim))
+                return False
+
+        return True
+
+    @log_entry_exit(LOG)
+    def validate_ns_allocated_vresources(self, ns_instance_id, additional_param=None):
+        ns_info = self.ns_query(filter={'ns_instance_id': ns_instance_id})
+        for vnf_info in ns_info.vnf_info:
+            if not self.validate_vnf_allocated_vresources(vnf_info):
+                return False
+        return True
+
+    @log_entry_exit(LOG)
+    def validate_vnf_allocated_vresources(self, vnf_info):
+        validation_result = True
+
+        vnfd_id = vnf_info.vnfd_id
+        vnfd = self.get_vnfd(vnfd_id)
+
+        expected_vdu_flavours = {}
+        for vdu in vnfd.get('vdu'):
+            vdu_id = str(vdu.get('id'))
+            expected_vdu_flavours[vdu_id] = list()
+            if bool(vdu.get('computation_requirement')):
+                vdu_flavour = str(vdu.get('computation_requirement'))
+                expected_vdu_flavours[vdu_id].append(vdu_flavour)
+            else:
+                for flavour in vnfd.get('deployment_flavour'):
+                    vdu_flavour = str(flavour.get('flavour_key'))
+                    expected_vdu_flavours[vdu_id].append(vdu_flavour)
+
+        for vnfc_resource_info in vnf_info.instantiated_vnf_info.vnfc_resource_info:
+            vdu_id = vnfc_resource_info.vdu_id
+
+            # Get VIM adapter object
+            vim = self.get_vim_helper(vnfc_resource_info.compute_resource.vim_id)
+
+            server_id = vnfc_resource_info.compute_resource.resource_id
+            server_details = vim.server_get(server_id)
+            server_flavour_id = server_details['flavor_id']
+            flavour_details = vim.flavor_get(server_flavour_id)
+            flavour_name_nova = str(flavour_details['name'])
+            if flavour_name_nova not in expected_vdu_flavours.get(vdu_id):
+                validation_result = False
+
+        return validation_result
+
+    @log_entry_exit(LOG)
+    def verify_vnf_nsd_mapping(self, ns_instance_id, additional_param=None):
+        validation_result = True
+
+        ns_info = self.ns_query(filter={'ns_instance_id': ns_instance_id, 'additional_param': additional_param})
+        nsd_id = ns_info.nsd_id
+        nsd = self.get_nsd(nsd_id)
+        expected_vnf_vnfd_mapping = dict()
+        for vnfd in nsd.get('vnfd'):
+            vnf_name = str(vnfd.get('name'))
+            expected_vnf_vnfd_mapping[vnf_name] = str(vnfd.get('id'))
+
+        ns_info = self.ns_query(filter={'ns_instance_id': ns_instance_id, 'additional_param': additional_param})
+        for vnf_info in ns_info.vnf_info:
+            vnf_name = vnf_info.vnf_product_name
+            if vnf_info.vnfd_id != expected_vnf_vnfd_mapping[vnf_name]:
+                return False
+
+        return validation_result
+
+    @log_entry_exit(LOG)
+    def get_nsd(self, nsd_id):
+        url = '/api/v1/ns-descriptors/%s' % nsd_id
+        method = 'get'
+        try:
+            resp, nsd = self.do_request(url=url, method=method)
+            assert resp.status_code == 200
+        except Exception as e:
+            LOG.exception(e)
+            raise OpenbatonManoAdapterError('Unable to retrieve config for NSD with ID %s. Reason: %s' % (nsd_id, e.message))
+        return nsd
+
+    @log_entry_exit(LOG)
+    def get_vnf_mgmt_addr_list(self, vnf_instance_id, additional_param=None):
+        mgmt_addr_list = list()
+        ns_instance_id = self.vnf_to_ns_mapping.get(vnf_instance_id)
+        url = '/api/v1/ns-records/%s/vnfrecords/%s' % (ns_instance_id, vnf_instance_id)
+        method = 'get'
+        try:
+            resp, vnf_config = self.do_request(url=url, method=method)
+            assert resp.status_code == 200
+        except Exception as e:
+            LOG.exception(e)
+            raise OpenbatonManoAdapterError('Unable to retrieve config for VNF with ID %s. Reason: %s' % (vnf_instance_id, e.message))
+        mgmt_addr_list = [str(addr) for addr in vnf_config.get('vnf_address')]
+
+        return mgmt_addr_list
