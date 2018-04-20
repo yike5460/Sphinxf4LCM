f96765c37f22c322e406890215c9fc4afc0d3efc
Alex Carp <acarp@luxoft.com>
2018-04-20 15:31:27 +0300

    #261: Modified vnf_query to populate vnf_ext_cp_info.address with dict

--- api/adapter/mano/cisconfv45.py
+++ api/adapter/mano/cisconfv45.py
@@ -925,7 +925,7 @@ class CiscoNFVManoAdapter(object):
                                                                 % (vm_group, vm_id_text, if_id_text))
                         ip_address_text = ip_address.text
 
-                        # Get the IP address
+                        # Get the MAC address
                         mac_address = opdata_deployment_xml.find('.//{http://www.cisco.com/esc/esc}vm_group'
                                                                  '[{http://www.cisco.com/esc/esc}name="%s"]/'
                                                                  '{http://www.cisco.com/esc/esc}vm_instance'
@@ -940,7 +940,10 @@ class CiscoNFVManoAdapter(object):
                         # Build the VnfExtCpInfo data structure
                         vnf_ext_cp_info = VnfExtCpInfo()
                         vnf_ext_cp_info.cp_instance_id = port_id_text
-                        vnf_ext_cp_info.address = [mac_address_text]
+                        vnf_ext_cp_info.address = {
+                            'ip': [ip_address_text],
+                            'mac': [mac_address_text]
+                        }
                         vnf_ext_cp_info.cpd_id = cpd_id_text
 
                         # Append the current VnfExtCpInfo element to the VnfExtCpInfo list
