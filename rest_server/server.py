df19f5aa050c45067ed111183eae69d0f4562651
Alex Carp <acarp@luxoft.com>
2018-05-17 15:20:26 +0300

    #321: Added step for dumping raw json in REST server

--- rest_server/server.py
+++ rest_server/server.py
@@ -98,6 +98,7 @@ def execute_test(tc_exec_request, tc_input, execution_queue, message_queue, step
     timestamp = '{:%Y_%m_%d_%H_%M_%S}'.format(datetime.now())
     log_file_name = '%s_%s.log' % (timestamp, str(tc_name))
     report_file_name = '%s_%s.txt' % (timestamp, str(tc_name))
+    json_file_name = '%s_%s.json' % (timestamp, str(tc_name))
 
     root_logger = logging.getLogger()
     logging_module.configure_logger(root_logger, file_level='DEBUG', log_filename=log_file_name)
@@ -121,6 +122,7 @@ def execute_test(tc_exec_request, tc_input, execution_queue, message_queue, step
         reporting.kibana_report(kibana_srv, tc_exec_request, tc_input, tc_result)
 
     reporting.report_test_case(report_file_name, tc_exec_request, tc_input, tc_result)
+    reporting.dump_raw_json(json_file_name, tc_exec_request, tc_input, tc_result)
 
 
 @route('/version')
